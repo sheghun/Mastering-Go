39718ae28d241962dc157c409a2e1354628fcb61
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2020-03-22 21:38:53 +0100

    Wrote a simple ticker

---
+++ section-5/timers_and_tickers/timers_ticker.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go tickCounter(2)
+	time.Sleep(5 * time.Second)
+}
+
+func tickCounter(n int) {
+	ticker := time.NewTicker(time.Duration(n) * time.Second)
+	i := 0
+	for t := range ticker.C {
+		i++
+		fmt.Println("Count ", i, " at ", t)
+
+	}
+}
